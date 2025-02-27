0c5dea4a8d6dfcfc6b6e724f3113e3dd44cfaf11
Giovanna Alves <giovanna.gigifa@gmail.com>
2025-02-27 00:37:46 -0300

    Delete frontend directory

--- frontend/src/states/Contants/SongConstant.jsx
+++
@@ -1,4 +0,0 @@
-export const PLAY_SONG_REQUEST = "PLAY_SONG_REQUEST";
-export const PAUSE_SONG_REQUEST = "PAUSE_SONG_REQUEST";
-export const PLAY_MASTER = "PLAY_MASTER";
-export const PAUSE_MASTER = "PAUSE_MASTER";
