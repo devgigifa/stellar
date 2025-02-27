f6cff827645b8aac7df04cf8aaf4583f0192cd45
Giovanna Alves <giovanna.gigifa@gmail.com>
2025-02-27 00:37:20 -0300

    Delete backend directory

--- backend/helper/generateToken.js
+++
@@ -1,8 +0,0 @@
-const jsonWeb = require("jsonwebtoken");
-
-exports.generateToken = async (id) => {
-  let token = jsonWeb.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: "24h", // expires in 24 hours
-  });
-  return token;
-};
