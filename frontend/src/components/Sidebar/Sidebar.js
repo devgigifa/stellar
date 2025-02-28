26daf2ae876f615178a3a4662087d79c5cd91914
Giovanna Alves <giovanna.gigifa@gmail.com>
2025-02-28 16:12:42 -0300

    Update Sidebar.js

--- frontend/src/components/Sidebar/Sidebar.js
+++ frontend/src/components/Sidebar/Sidebar.js
@@ -1,11 +1,13 @@
+// components/Sidebar/Sidebar.js
 import React from 'react';
 import styled from 'styled-components';
-import { LuLibrary } from "react-icons/lu";
+import { LuLibrary } from 'react-icons/lu';
+import { Link } from 'react-router-dom';
 
 const SidebarContainer = styled.div`
   flex: 0.5;
-  overflow-y: auto;  
-  padding: 20px;  
+  overflow-y: auto;
+  padding: 20px;
   border-radius: 10px;
   background-color: #121212;
 `;
@@ -59,87 +61,33 @@ const SidebarText = styled.div`
   }
 `;
 
-function Sidebar() {
+const Sidebar = () => {
+  const playlists = [
+    { id: 1, name: 'Playlist 1', description: 'Descrição da playlist 1', songs: [{ name: 'Song 1', artist: 'Artist 1' }, { name: 'Song 2', artist: 'Artist 2' }] },
+    { id: 2, name: 'Playlist 2', description: 'Descrição da playlist 2', songs: [{ name: 'Song 3', artist: 'Artist 3' }, { name: 'Song 4', artist: 'Artist 4' }] },
+    { id: 3, name: 'Playlist 3', description: 'Descrição da playlist 3', songs: [{ name: 'Song 5', artist: 'Artist 5' }, { name: 'Song 6', artist: 'Artist 6' }] },
+  ];
+
   return (
     <SidebarContainer>
       <SidebarTitle>
-      <LuLibrary /> Sua Biblioteca
-      </SidebarTitle>      
+        <LuLibrary /> Sua Biblioteca
+      </SidebarTitle>
       <SidebarList>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        <SidebarItem>
-          <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt="Playlist"/>
-          <SidebarText>
-            <p>Playlist</p>
-            <span>Playlist • Você</span>
-          </SidebarText>
-        </SidebarItem>
-        {/* Adicione outros itens aqui */}
+        {playlists.map((playlist) => (
+          <Link to={`/playlist/${playlist.id}`} key={playlist.id}>
+            <SidebarItem>
+              <SidebarImage src="https://cdn.venngage.com/template/thumbnail/small/bf008bfe-9bf6-4511-b795-e86f070bfff5.webp" alt={playlist.name} />
+              <SidebarText>
+                <p>{playlist.name}</p>
+                <span>{playlist.name} • Você</span>
+              </SidebarText>
+            </SidebarItem>
+          </Link>
+        ))}
       </SidebarList>
     </SidebarContainer>
   );
-}
+};
 
 export default Sidebar;
